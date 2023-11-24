46b284294f5d0509e37f99659de0a8b239961bdd
Vadim <maryushkin.vadim@gmail.com>
2023-11-24 20:16:12 +0300

    Тестовая загрузка

---
+++ PraktikaGit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.ComponentModel.Design;
+
+namespace Praktika
+{
+    public class itProger
+    {
+
+        public static void Main()
+        {
+         Console.WriteLine("Практика по Git");
+        }
+
+
+
+    }
+}
+
